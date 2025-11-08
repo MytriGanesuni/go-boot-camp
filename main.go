f2f65a7840af3f237594d75442b6a2716991f289
MytriGanesuni <mytri.ganesuni@rapido.bike>
2025-11-08 10:15:20 +0530

    Broken down the ratings into struct & function

--- main.go
+++ main.go
@@ -1,36 +1,48 @@
 package main
 
-import "fmt"
-import "github.com/fatih/color"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+type Rating struct {
+	Rating   int
+	Comment  string
+	Customer string
+}
+
+func (r *Rating) GetStars() string {
+	return strings.Repeat("⭐", r.Rating)
+}
+
+func (r *Rating) CollectInput() {
+	fmt.Println("Enter your name:")
+	fmt.Scanln(&r.Customer)
+	fmt.Println("Enter your rating:")
+	fmt.Scanln(&r.Rating)
+	fmt.Println("Enter your comment:")
+	fmt.Scanln(&r.Comment)
+}
+
+func (r *Rating) Display() {
+	stars := r.GetStars()
+	if r.Rating >= 3 {
+		color.Green("Thank you %s for your rating %s and your comment: %s", r.Customer, stars, r.Comment)
+	} else {
+		color.Red("Sorry to hear about the rating %s, we will improve our service.", stars)
+	}
+}
 
 func main() {
+	rating := &Rating{}
+	rating.CollectInput()
 
-	type rating struct {
-		rating   int
-		comment  string
-		customer string
+	if rating.Rating < 1 || rating.Rating > 5 {
+		color.Red("Invalid rating. Please enter a rating between 1 and 5.")
+		return
 	}
 
-	ratings := rating{}
-
-	fmt.Println("Enter your name:")
-	fmt.Scanln(&ratings.customer)
-
-	fmt.Println("Enter your rating:")
-	fmt.Scanln(&ratings.rating)
-
-	fmt.Println("Enter your comment:")
-	fmt.Scanln(&ratings.comment)
-
-	stars := ""
-	for i := 0; i < ratings.rating; i++ {
-		stars += "⭐"
-	}
-
-	if(ratings.rating >=3 ){
-		color.Green("Thank you ",ratings.customer," for your rating", stars, "and your comment", ratings.comment)
-	} else {
-		color.Red("Sorry to hear about the rating ", stars ," ,we will improve our service.")
-	}
-
+	rating.Display()
 }
