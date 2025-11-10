84c431181e9145df08c0a1be747131ff25ab2ea3
MytriGanesuni <mytri.ganesuni@rapido.bike>
2025-11-10 12:02:47 +0530

    Moved ratings fucntion into a new package

--- main.go
+++ main.go
@@ -2,47 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/fatih/color"
+	"hello_world/ratings"
 )
 
-type Rating struct {
-	Rating   int
-	Comment  string
-	Customer string
-}
-
-func (r *Rating) GetStars() string {
-	return strings.Repeat("⭐", r.Rating)
-}
-
-func (r *Rating) CollectInput() {
-	fmt.Println("Enter your name:")
-	fmt.Scanln(&r.Customer)
-	fmt.Println("Enter your rating:")
-	fmt.Scanln(&r.Rating)
-	fmt.Println("Enter your comment:")
-	fmt.Scanln(&r.Comment)
-}
-
-func (r *Rating) Display() {
-	stars := r.GetStars()
-	if r.Rating >= 3 {
-		color.Green("Thank you %s for your rating %s and your comment: %s", r.Customer, stars, r.Comment)
-	} else {
-		color.Red("Sorry to hear about the rating %s, we will improve our service.", stars)
-	}
-}
-
 func main() {
-	rating := &Rating{}
-	rating.CollectInput()
 
-	if rating.Rating < 1 || rating.Rating > 5 {
-		color.Red("Invalid rating. Please enter a rating between 1 and 5.")
-		return
+	rating := &ratings.Rating{}
+	err := rating.AddUserRating("Customer1", 8.0, "Great product")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = rating.AddUserRating("Customer2", 4.0, "okay")
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	rating.Display()
+	fmt.Println(rating)
+
 }
---
+++ ratings/ratings.go
@@ -0,0 +1,57 @@
+package ratings
+
+import "time"
+import "fmt"
+import "errors"
+
+type Rating struct {
+	id          int
+	userRatings []UserRatings
+	avgRating   float64
+}
+
+type Comment struct {
+	date time.Time
+	text string
+}
+
+type UserRatings struct {
+	customer string
+	rating   float64
+	comments []Comment
+}
+
+func (r *Rating) AddUserRating(customer string, rating float64, comment string) error{
+	if rating < 0 || rating > 5 {
+		return errors.New("rating must be between 0 and 5")
+	}
+	r.userRatings = append(r.userRatings, UserRatings{
+		customer: customer,
+		rating:   rating,
+		comments: []Comment{
+			{date: time.Now(),
+				 text: comment},
+		},
+	})
+	r.avgRating = r.AverageRating()
+	return nil
+}
+
+func (receiver Comment) String() string {
+	return fmt.Sprintf("Date : %s, Text: %s", receiver.date.Format("06-01-02 15:04:05"), receiver.text)
+}
+func (receiver UserRatings) String() string {
+	return fmt.Sprintf("Customer: %s, Rating: %.2f, Comments: %v", receiver.customer, receiver.rating, receiver.comments)
+}
+func (receiver Rating) String() string {
+	return fmt.Sprintf("User Ratings: %v, Average Rating: %.2f", receiver.userRatings, receiver.avgRating)
+}
+
+func (receiver Rating) AverageRating() float64 {
+
+	totalRating := 0.0
+	for _, userRating := range receiver.userRatings {
+		totalRating += userRating.rating
+	}
+	return totalRating / float64(len(receiver.userRatings))
+}
